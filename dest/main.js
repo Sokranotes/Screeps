1d36efe40250119c0c08c9aa06fd0ff9261c165c
MusicBear <zengwuxu@gmail.com>
2021-09-10 15:21:55 +0800

    Hello, rollup.
    construct program with rollup
    
    Thanks HoPGoldy.
    
    https://www.jianshu.com/p/13e2cbcb60ab

---
+++ dest/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+// 游戏入口函数
+const loop = function () {
+    console.log('hello world');
+};
+
+exports.loop = loop;
+//# sourceMappingURL=main.js.map
---
+++ src/main.js
@@ -0,0 +1,4 @@
+// 游戏入口函数
+export const loop = function () {
+    console.log('hello world')
+}
