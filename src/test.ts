b6b937dbf7f7ee29f8504f4b2f3b28cff7bb1032
Sokranotes <zengwuxu@gmail.com>
2021-10-06 21:51:36 +0800

    test move

---
+++ src/test.ts
@@ -0,0 +1,8 @@
+export const test_work = function(creep: Creep){
+    // if (creep.name == 'Repairer32103476')
+    //     console.log(creep.move(TOP))
+    // if (creep.name == 'Repairer32103192')
+    //     console.log(creep.move(BOTTOM))
+
+    // 哪怕相撞，也会走过去，但是都返回OK
+}
