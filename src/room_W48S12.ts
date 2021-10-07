d2afd3bbf7eed75c9994774af186b2aaf72f5fe4
Sokranotes <zengwuxu@gmail.com>
2021-10-07 18:43:56 +0800

    Basic functions of low-level rooms

--- src/room_W48S12.ts
+++ src/room_W48S12.ts
@@ -1,10 +1,18 @@
 export const room_W48S12_running = function(roomName: string){
     let spawn_name = 'Spawn2'
-    let home: Room = Game.rooms[roomName]
+    // let home: Room = Game.rooms[roomName]
     let upgradersNum: number = 4;
+    let buildersNum: number = 2;
+    if (Game.rooms['W48S12'].find(FIND_CONSTRUCTION_SITES).length == 0){
+        buildersNum = 0
+    }
+    let repairersNum: number = 1;
     
     let upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-    let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    let builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+    let repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
+    let harvester0s = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester' && creep.memory.source_idx == 0);
+    let harvester1s = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester' && creep.memory.source_idx == 1);
     if (Game.spawns[spawn_name].spawning){
         let spawningCreep = Game.creeps[Game.spawns[spawn_name].spawning.name];
         Game.spawns[spawn_name].room.visual.text(
@@ -17,8 +25,20 @@ export const room_W48S12_running = function(roomName: string){
         let newName = 'Upgrader' + Game.time;
         Game.spawns[spawn_name].spawnCreep([WORK, WORK, CARRY, MOVE], newName, {memory: {role: 'upgrader', source_idx: 0}});
     }
-    else if (harvesters.length < upgradersNum){
+    else if (harvester0s.length < upgradersNum){
         let newName = 'Harvester' + Game.time;
         Game.spawns[spawn_name].spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {memory: {role: 'harvester', source_idx: 0}});
+    }
+    else if (harvester1s.length < upgradersNum){
+        let newName = 'Harvester' + Game.time;
+        Game.spawns[spawn_name].spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {memory: {role: 'harvester', source_idx: 1}});
+    }
+    else if (builders.length < buildersNum){
+        let newName = 'Builder' + Game.time;
+        Game.spawns[spawn_name].spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {memory: {role: 'builder', source_idx: 1}});
+    }
+    else if (repairers.length < repairersNum){
+        let newName = 'Repairer' + Game.time;
+        Game.spawns[spawn_name].spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {memory: {role: 'harvester', source_idx: 1}});
     }
 }
