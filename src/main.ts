af835ff71dddc4b80b378230a7fe0cec1c0fa24c
Wuxu Zeng <zengwuxu@gmail.com>
2021-10-27 23:34:48 +0800

    energy state diagram and pixel

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,16 @@ import { mainSokranotes } from './Sokranotes/mainSokranotes';
 import { mainUniversal } from './Universal/mainUniversal';
 
 export const loop = errorMapper(() => {
+    if (Game.shard.name == 'shard2'){
+        if(Game.cpu.bucket == 10000) {
+            Game.cpu.generatePixel();
+        }
+        return
+    }
     if (Game.flags.Appassionata){
+        if(Game.cpu.bucket == 10000) {
+            Game.cpu.generatePixel();
+        }
         mainUniversal()
     }
     else{
